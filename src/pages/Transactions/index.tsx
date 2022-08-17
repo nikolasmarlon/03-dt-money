51b45674e05b42dcc874c6a05d249c82398b8da0
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2022-08-17 12:34:54 -0300

    Criando contexto de transações
    Context API react

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,39 +1,19 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Header/Summary";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 
-interface Transaction {
-    id: number
-    description: string
-    type: 'income' | 'outcome'
-    price: number
-    category: string
-    createdAt: string
-}
+
 
 export function Transactions(){
 
 
 
-    const [transactions, setTransactions] = useState<Transaction[]>([])
-
-    //promises
-    // fetch executa sempre que a pagina é recalculada(renderizada)
-    //para contornar vamos usar o useEffect
-
-    async function loadTransactions(){
-        const response = await fetch('http://localhost:3000/transactions')
-        const data = await response.json()
-
-        setTransactions(data)
-    } 
-    
-    useEffect(() => {
-        loadTransactions()
-    }, [])
+    const { transactions } = useContext(TransactionsContext)
     
 
     return(
