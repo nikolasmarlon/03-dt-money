99b4a2f0399fb90b159f59818a136baad39ad668
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2022-08-19 21:15:07 -0300

    Criando hook para o resumo

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -4,36 +4,12 @@ import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar} from 'phosphor-react'
 import { useContext } from "react";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 
 export function Summary() {
 
-    const { transactions } = useContext(TransactionsContext)
-
-    //reduce permite percorrer o array e reduzir este array a uma nova estrutura de dados
-    //neste caso, vai converter o array de  transactions num objeto -- {income: 0, outcome: 0, total: 0}
-
-    //Primeiro parâmetro é uma função, segundo é a estrutura de dados inicial
-    const summary = transactions.reduce(
-        //as operações sao feitas no accumulator(resumo)
-        (acc, transaction) => {
-            if (transaction.type === 'income'){
-                acc.income += transaction.price
-                acc.total = acc.total + transaction.price
-                
-            }else {
-                acc.outcome += transaction.price
-                acc.total = acc.total - transaction.price
-            }
-
-            return acc
-        }, 
-        {
-            income: 0, 
-            outcome: 0, 
-            total: 0
-        }
-        )
+   const summary = useSummary()
 
     return (
         <SummaryContainer>
---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,37 @@
+import { useContext } from "react"
+import { TransactionsContext } from "../contexts/TransactionsContext"
+
+export function useSummary() {
+
+const { transactions } = useContext(TransactionsContext)
+
+//reduce permite percorrer o array e reduzir este array a uma nova estrutura de dados
+//neste caso, vai converter o array de  transactions num objeto -- {income: 0, outcome: 0, total: 0}
+
+//Primeiro parâmetro é uma função, segundo é a estrutura de dados inicial
+const summary = transactions.reduce(
+    //as operações sao feitas no accumulator(resumo)
+    (acc, transaction) => {
+        if (transaction.type === 'income'){
+            acc.income += transaction.price
+            acc.total = acc.total + transaction.price
+            
+        }else {
+            acc.outcome += transaction.price
+            acc.total = acc.total - transaction.price
+        }
+
+        return acc
+    }, 
+    {
+        income: 0, 
+        outcome: 0, 
+        total: 0
+    }
+
+        
+    )
+
+    return summary
+
+}
